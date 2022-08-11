f2f2f3ef74eb38245eb26c15c95a3eb4f4d4dd75
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2022-08-11 10:45:13 -0500

    add the NewCard

--- src/commons/NewCard/NewCard.tsx
+++ src/commons/NewCard/NewCard.tsx
@@ -11,6 +11,41 @@ import {
 } from "@chakra-ui/react"
 import { Dispatch, SetStateAction } from "react"
 
+const TitleInput = styled.input`
+  border: 1px solid black;
+  margin-bottom: 15px;
+  border-radius: 5px;
+`
+
+const DescriptionTextArea = styled.textarea`
+  border: 1px solid black;
+  width: 100%;
+  height: 200px;
+  border-radius: 5px;
+`
+
+const CancelButton = styled.button`
+  width: 80px;
+  border-radius: 5px;
+  background-color: #eb5252;
+  color: white;
+  padding-left: 5px;
+  padding-right: 5px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+`
+
+const SumbitButton = styled.button`
+  width: 80px;
+  border-radius: 5px;
+  background-color: var(--secondary-color);
+  color: white;
+  padding-left: 5px;
+  padding-right: 5px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  margin-left: 10px;
+`
 export interface NewCardProps {
   isOpen: boolean
   setIsOpen: Dispatch<SetStateAction<boolean>>
@@ -20,16 +55,20 @@ export const NewCard = ({ isOpen, setIsOpen }: NewCardProps) => {
   return (
     <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
       <ModalOverlay />
-      <ModalContent maxH={"full"} color="white">
-        <ModalHeader>Modal Title</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>ssssssssssssssssssssssssssssssssssss</ModalBody>
+      <ModalContent px={4} py={4}>
+        <ModalHeader p={0} mb={4}>
+          New Card
+        </ModalHeader>
+        <h1>Add Title</h1>
+        <TitleInput />
+        <ModalBody p={0}>
+          <h1>Description</h1>
+          <DescriptionTextArea />
+        </ModalBody>
 
-        <ModalFooter>
-          <Button colorScheme="blue" mr={3} onClick={() => setIsOpen(false)}>
-            Close
-          </Button>
-          <Button variant="ghost">Secondary Action</Button>
+        <ModalFooter p={0} my={3}>
+          <CancelButton onClick={() => setIsOpen(false)}>Cancel</CancelButton>
+          <SumbitButton>Submit</SumbitButton>
         </ModalFooter>
       </ModalContent>
     </Modal>
