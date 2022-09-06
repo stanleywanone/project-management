2d1ce6982f6a2d79b0f14f96afb796b4499b2b19
Hsing-Hung Wan <hsing-hungwan@Advantiss-MacBook-Pro-2.local>
2022-09-06 12:07:32 -0500

    add the redirect to /auth/signin

--- src/components/Router/components/Routers.tsx
+++ src/components/Router/components/Routers.tsx
@@ -1,16 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Auth from "../../Auth/components/Auth"
-import { Projects } from "../../Projects/components/Projects"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Auth from "../../Auth/components/Auth";
+import { Projects } from "../../Projects/components/Projects";
 
 export const Routers = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="" element={<Navigate to="/auth/signin" />} />
         <Route path="/auth/signin" element={<Auth />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default Routers
+export default Routers;
